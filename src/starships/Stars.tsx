162ea0ab65894eb95f6905adc370f7fd65e1f771
Toby Rumans <toby.rumans@gmail.com>
2021-09-05 16:02:25 -0600

    Render star svgs

--- src/starships/Stars.tsx
+++ src/starships/Stars.tsx
@@ -1,7 +1,23 @@
+import star from "../assets/images/full_star.svg"
+import halfStar from "../assets/images/half_star.svg"
+import "./Stars.scss"
+
 interface StarsProps {
   count: string
 }
 
+
+function renderStars(n: number) {
+  const decimal = n - Math.floor(n);
+  const hasDecimal = decimal !== 0;
+
+  const stars = Array(n - decimal)
+    .fill("")
+    .map((_, i) => <img key={i} alt="full-star" src={star}/> );
+  return hasDecimal ? stars : [...stars, <img alt="half-star" src={halfStar}/>]
+}
+
 export const Stars = ({count}: StarsProps) => {
-  return <div>{count}</div>
+  const number = parseFloat(count);
+  return <span className="Stars">{renderStars(number)}</span>
 }
